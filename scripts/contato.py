3eacb29a752af601d7ab1749096fee6b008f5bcb
Luis Gabriel <luisga.carv@gmail.com>
2025-04-15 10:51:46 -0300

    contato.py

---
+++ scripts/contato.py
@@ -0,0 +1,9 @@
+f exibir_contato():
+    """Função que exibe informações de contato."""
+    print("Contato:")
+    print("Email: luisga.carv@gmail.com")
+    print("LinkedIn: linkedin.com/in//luisgabrielmartins")
+    print("GitHub: github.com/luisgacarv")
+
+if __name__ == "__main__":
+    exibir_contato()
